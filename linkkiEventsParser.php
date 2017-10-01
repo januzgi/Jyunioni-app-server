4dd8958e4f5e602ebe7c364558508c186c48463d
januzgi <janisuoranta@icloud.com>
2017-10-01 19:20:04 +0300

    Starting Linkki Event's detail parsing in PHP
    Event's count working along with line by line reading through the file.

--- linkkiEventsParser.php
+++ linkkiEventsParser.php
@@ -24,14 +24,116 @@ $LINKKI_NEXT_MONTH_EVENTS_URL .= $nextMonth;
 $thisContent = file_get_contents($LINKKI_THIS_MONTH_EVENTS_URL);
 $nextContent = file_get_contents($LINKKI_NEXT_MONTH_EVENTS_URL);
 
-$thisMonthFile = '/Users/JaniS/Sites/Jyunioni/linkkiThisMonthRawEventData.txt';
-$nextMonthFile = '/Users/JaniS/Sites/Jyunioni/linkkiNextMonthRawEventData.txt';
+$thisMonthFile = '/Users/JaniS/Sites/Jyunioni server/linkkiThisMonthRawEventData.txt';
+$nextMonthFile = '/Users/JaniS/Sites/Jyunioni server/linkkiNextMonthRawEventData.txt';
 
-// Write the contents back to the file
-file_put_contents($thisMonthFile, $thisContent);
-file_put_contents($nextMonthFile, $nextContent);
 
-echo "Following files written succesfully:<br>" . $thisMonthFile . "<br>" . $nextMonthFile;
+// Write the contents back to a .txt file
+echo "Following files written succesfully:<br>";
+
+if (file_put_contents($thisMonthFile, $thisContent) !== FALSE){
+	echo $thisMonthFile . "<br>";
+}
+
+if (file_put_contents($nextMonthFile, $nextContent) !== FALSE){
+	echo $nextMonthFile . "<br>";
+}
+
+
+
+
+
+
+# Read the text file line by line and parse the different Event fields.
+
+# Event's count in a file
+$eventsCount = 0;
+# Event's beginning symbol
+$eventBegin = "BEGIN:VEVENT";
+
+# Count the amount of Events in the file. Open the file for reading only.
+$handle = @fopen("$thisMonthFile", "r");
+if ($handle) {
+
+    while (($buffer = fgets($handle)) !== FALSE) {
+
+    	// If the line contains "BEGIN:VEVENT"
+        if (strpos($buffer, $eventBegin) !== FALSE){
+        	$eventsCount++;
+        }
+    }
+
+    if (!feof($handle)) {
+        echo "Error: unexpected fgets() fail.<br>";
+    }
+
+    fclose($handle);
+}
+
+
+
+# Open the file for reading only. Get the different variable values from the file
+$handle = @fopen("$thisMonthFile", "r");
+if ($handle) {
+
+	// Create the Event object variables
+	// Event imageId and groupColorId will be added locally in the phone
+	$eventTimeStart = "";
+    $eventTimeEnd = "";
+	$eventTimestamp = "";
+
+	$eventName = "";
+	$eventInformation = "";
+	$eventUrl = "";
+
+
+	// Keep reading until end of line and check if there are Events in the file.
+    while (($buffer = fgets($handle)) !== FALSE && $eventsCount !== 0) {
+
+
+    	for ($i = 0; $i < $eventsCount; $i++){
+
+    		if (strpos($buffer, "DTSTART;") !== FALSE){
+    			$eventTimeStart = $buffer;
+    			echo $eventTimeStart . "<br>";
+    		}
+
+
+    	}
+
+
+
+        echo $buffer . "<br>";
+    }
+
+
+    // In case of an error with the fgets() method
+    if (!feof($handle)) {
+        echo "Error: unexpected fgets() fail.<br>";
+    }
+
+    // Close the handle from taking resources
+    fclose($handle);
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 ?>
+
+
+
+
+
+
+
+
