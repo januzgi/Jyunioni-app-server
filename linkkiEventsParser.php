494c8a7dccf83ac031b7bb260fcea3332e510ba9
januzgi <janisuoranta@icloud.com>
2017-10-04 16:23:19 +0300

    Nested loop's for fetching the data
    Data gets fetched from .txt files. Next step to add the data to arrays
    and get use of the arrays.

--- linkkiEventsParser.php
+++ linkkiEventsParser.php
@@ -29,36 +29,55 @@ $nextMonthFile = '/Users/JaniS/Sites/Jyunioni server/linkkiNextMonthRawEventData
 
 
 // Write the contents back to a .txt file
-echo "Following files written succesfully:<br>";
+echo "Following files written succesfully:<br><br>";
 
-if (file_put_contents($thisMonthFile, $thisContent) !== FALSE){
+if (file_put_contents($thisMonthFile, $thisContent) !== false){
 	echo $thisMonthFile . "<br>";
 }
 
-if (file_put_contents($nextMonthFile, $nextContent) !== FALSE){
-	echo $nextMonthFile . "<br>";
+if (file_put_contents($nextMonthFile, $nextContent) !== false){
+	echo $nextMonthFile . "<br><br>";
 }
 
-
-
+echo "################### ################### ###################" . "<br>" . "Output:" . "<br>" . "################### ################### ###################" . "<br><br>";
 
 
 
 # Read the text file line by line and parse the different Event fields.
+extractEventsData($thisMonthFile);
+extractEventsData($nextMonthFile);
 
+
+// Variables for the event's extracting
 # Event's count in a file
 $eventsCount = 0;
 # Event's beginning symbol
 $eventBegin = "BEGIN:VEVENT";
 
+	// Create the Event object variables
+	// Event imageId and groupColorId will be added locally in the phone
+	$eventTimeStart = "";
+    $eventTimeEnd = "";
+	$eventTimestamp = "";
+
+	$eventName = "";
+	$eventInformation = "";
+	$eventUrl = "";
+
+	// Create the array for the extracted event data
+	$extractedEventsData = array();
+
+
+function extractEventsData($file){
+
 # Count the amount of Events in the file. Open the file for reading only.
-$handle = @fopen("$thisMonthFile", "r");
+$handle = @fopen("$file", "r");
 if ($handle) {
 
-    while (($buffer = fgets($handle)) !== FALSE) {
+    while (($buffer = fgets($handle)) !== false) {
 
     	// If the line contains "BEGIN:VEVENT"
-        if (strpos($buffer, $eventBegin) !== FALSE){
+        if (strpos($buffer, $eventBegin) !== false){
         	$eventsCount++;
         }
     }
@@ -71,39 +90,87 @@ if ($handle) {
 }
 
 
-
-# Open the file for reading only. Get the different variable values from the file
-$handle = @fopen("$thisMonthFile", "r");
+# Open the file for reading only. Get the different event values from the file
+$handle = @fopen("$file", "r");
 if ($handle) {
 
-	// Create the Event object variables
-	// Event imageId and groupColorId will be added locally in the phone
-	$eventTimeStart = "";
-    $eventTimeEnd = "";
-	$eventTimestamp = "";
+	// Keep reading until end of line and check if there are Events in the file. Also check that the for -loop hasn't looped as many times as there
+	// are events in the file.
+    while (($buffer = fgets($handle)) !== false && $eventsCount !== 0) {
 
-	$eventName = "";
-	$eventInformation = "";
-	$eventUrl = "";
+    	// Set maximum execution time so in case of an erronous run it doesn't loop 30 seconds (default)
+   		ini_set('max_execution_time', 1);
 
-
-	// Keep reading until end of line and check if there are Events in the file.
-    while (($buffer = fgets($handle)) !== FALSE && $eventsCount !== 0) {
-
-
+    	// Loop through all the separate event's in the file.
+    	// Find Event fields in order: startTime, endTime, eventName, eventInformation, eventUrl
     	for ($i = 0; $i < $eventsCount; $i++){
 
-    		if (strpos($buffer, "DTSTART;") !== FALSE){
-    			$eventTimeStart = $buffer;
-    			echo $eventTimeStart . "<br>";
+    		// If the calendar ends
+			if (strpos($buffer, "END:VCALENDAR") !== false) break;
+
+    		while (true) {
+    			if (strpos($buffer, "DTSTART;") !== false){
+    				// Parse event starting time
+    				$eventTimeStart = $buffer; 
+    				// echo $eventTimeStart . "<br>";
+
+    				while (true) {
+    					if (strpos($buffer, "DTEND;") !== false){
+    						// Parse event ending time
+    						$eventTimeEnd = $buffer;
+    						// echo $eventTimeEnd . "<br>";
+    						
+    						while (true) {
+    							if (strpos($buffer, "SUMMARY:") !== false){
+    								$eventName = $buffer;
+    								// echo $eventName . "<br>";
+
+    								while (true) {
+    									if (strpos($buffer, "DESCRIPTION:") !== false){
+    										$eventInformation = $buffer;
+    										// echo $eventInformation . "<br>";
+
+    										// Event's URL
+    										while (true) {
+    											if (strpos($buffer, "URL") !== false){
+    												$eventUrl = $buffer;
+    												// echo $eventUrl . "<br><br><br>";
+
+    												// Event information ends on the next line so hop to it and break the for loop back to the while loop.
+    												$buffer = fgets($handle);
+    												break;
+
+    											}
+    											$buffer = fgets($handle);
+
+    										}
+    										break; 
+
+    									}
+    									$buffer = fgets($handle);
+    								}
+    								break;
+
+    							}
+    							$buffer = fgets($handle);
+    						}
+    						break;
+
+    					}
+    					$buffer = fgets($handle);
+    				}
+    				break;
+
+    			}
+    			$buffer = fgets($handle);
     		}
 
+    		// Add the event's data to the list
 
+
+    		echo "<br>" . $eventTimeStart . "<br>" . $eventTimeEnd . "<br>" . $eventName . "<br>" . $eventInformation . "<br>" . $eventUrl . "<br>";
+    		break;
     	}
-
-
-
-        echo $buffer . "<br>";
     }
 
 
@@ -114,17 +181,13 @@ if ($handle) {
 
     // Close the handle from taking resources
     fclose($handle);
+
+
+    // return or do something with the results.
 }
 
 
-
-
-
-
-
-
-
-
+}
 
 
 
