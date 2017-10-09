7eb0a57dd37451dd59ebf12333357bfbb53e3a85
januzgi <janisuoranta@icloud.com>
2017-10-09 19:46:31 +0300

    Porssi events parsing progressing...
    Very messy still. Problems with the DOMNode usage and how to get the
    relevant information out of the HTML. Going forward still!

--- porssiEventsParser.php
+++ porssiEventsParser.php
@@ -1,5 +1,7 @@
 <?php
 
+// http://localhost/~JaniS/Jyunioni%20server/porssiEventsParser.php
+
 # URL
 $PORSSI_EVENTS_URL = "http://www.porssiry.fi/tapahtumat/";
 
@@ -19,15 +21,46 @@ $porssiUrlsJson = file_get_contents("/Users/JaniS/Sites/Jyunioni server/Raw even
 // Decode the .json into an array
 $porssiEventUrls = json_decode($porssiUrlsJson, true);
 
-// Fetch each event's data from the event's page
-foreach ($porssiEventUrls as $url) {
-	echo "<pre>" . print_r($url, true) . "</pre>";
+
+// Get the raw event data into porssiRawEventData.txt
+getRawEventData($porssiEventUrls);
 
 
+
+// Fetch each event's raw data from the event's page
+function getRawEventData($urls)
+{
+    // All different events data will be put into this string
+    $eventPagesContents = "";
+    
+    
+    foreach ($urls as $url) {
+        // Get the content of the site
+        $html = file_get_contents($url);
+        
+        // Create a new DOMDocument instance
+        $dom = new DOMDocument();
+        
+        // Surpress errors with the '@' and load the String containing the page's HTML to the DOMDocument object
+        @$dom->loadHTML($html);
+        
+        // More about the xpath query usage: http://www.the-art-of-web.com/php/html-xpath-query/
+        $xpath    = new DOMXpath($dom);
+        $contents = $xpath->query('//div[@id="content"]');
+        // $contents is an instance of DOMNodeList
+        
+        // https://stackoverflow.com/questions/15807314/how-to-convert-domnodelist-object-into-array
+        
+        foreach ($contents as $item) {      
+            // Put the node's content to a string
+            $eventPagesContents .= $item->nodeValue;
+            $eventPagesContents .= $url;
+        }
+    }
+    
+    echo "<pre>" . print_r($eventPagesContents, true) . "</pre>";
+    
 }
-
-
-
 
 
 
