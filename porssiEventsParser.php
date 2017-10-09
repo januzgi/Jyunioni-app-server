079087792131466fa9f202abcad009ce3b29ba2d
januzgi <janisuoranta@icloud.com>
2017-10-09 17:47:44 +0300

    porssiEventsParser modifying
    Some formatting and rearranging.

--- porssiEventsParser.php
+++ porssiEventsParser.php
@@ -4,28 +4,27 @@
 $PORSSI_EVENTS_URL = "http://www.porssiry.fi/tapahtumat/";
 
 // Get Porssi's "css-events-list" HTML div's URL list to a porssiRawUrlData.json file
+// The event's page list in http://www.porssiry.fi/tapahtumat/ goes to page 2 after 20 events.
+// So only fetch the 20 first and not the url "http://www.porssiry.fi/tapahtumat/?pno=2"
 fetchUrls($PORSSI_EVENTS_URL);
 
 
 function fetchUrls($url)
 {
-    
     // Get the content of the site
     $html = file_get_contents($url);
     
     // Create a new DOMDocument instance
     $dom = new DOMDocument();
     
-    // We need to validate our document before refering to the id
-    $dom->validateOnParse = true;
-    
     // Surpress errors with the '@' and load the String containing the pages HTML to the DOMDocument object
     @$dom->loadHTML($html);
     
     
-    // http://www.the-art-of-web.com/php/html-xpath-query/
+    // More about the xpath query usage: http://www.the-art-of-web.com/php/html-xpath-query/
     $xpath    = new DOMXpath($dom);
-    $articles = $xpath->query('.//div[@class="css-events-list"]'); //instance of DOMNodeList
+    $articles = $xpath->query('.//div[@class="css-events-list"]');
+    // $articles is an instance of DOMNodeList
     
     // Get all links in div with id "css-events-list". Create an array instance.
     $links = array();
@@ -36,7 +35,7 @@ function fetchUrls($url)
         // Put the elements content (the URL's) from href fields to an array
         foreach ($arr as $item) {
             $href = $item->getAttribute("href");
-
+            
             // Add each link to the array
             array_push($links, $href);
         }
@@ -51,37 +50,19 @@ function fetchUrls($url)
             // Delete possible second link to the second page from the links list.
             array_pop($links);
         }
-
-        // Write the array of links into the .json file
-        $fp = fopen('/Users/JaniS/Sites/Jyunioni server/Raw event data/porssiRawUrlData.json', 'w');
-        if (fwrite($fp, json_encode($links, JSON_PRETTY_PRINT)) !== false) {
-        	echo "porssiRawUrlData.json written succesfully." . "<br>";
-        }
-        fclose($fp);
     }
+    
+    // Write the array of links into the .json file
+    $fp = fopen('/Users/JaniS/Sites/Jyunioni server/Raw event data/porssiRawUrlData.json', 'w');
+    if (fwrite($fp, json_encode($links, JSON_PRETTY_PRINT)) !== false) {
+        echo "porssiRawUrlData.json written succesfully." . "<br>";
+    }
+    fclose($fp);
 }
 
 
 
 /*
-if (requestUrl[i].contains("porssiry.fi")) {
-
-// Get just the "css-events-list" HTML div's data from Pörssi's website using jsoup library.
-* jsoup HTML parser library @ https://jsoup.org
-try {
-
-Document documentPorssi = Jsoup.connect(requestUrl[i]).get();
-
-* https://jsoup.org/cookbook/extracting-data/selector-syntax
-Elements porssiEventUrlElements = documentPorssi.getElementsByClass("css-events-list").select("[href]");
-
-// Put the elements content (the URL's) from href fields to a String List
-for (Element element : porssiEventUrlElements) {
-porssiEventUrls.add(element.attr("href"));
-}
-} catch (IOException e) {
-Log.e(LOG_TAG, "Problem in jsouping Pörssi Ry's events.\n" + e);
-}
 
 Event porssiEvent = null;
 
